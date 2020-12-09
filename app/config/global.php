ec6e9c8edd9d882d9c79f3ca188938a02c025dd5
joseedusantana <37674403+joseedusantana@users.noreply.github.com>
2020-12-08 21:50:13 -0300

    Aula 08 - OK

--- app/config/global.php
+++ app/config/global.php
@@ -1,4 +1,4 @@
-<?php>
+<?php
 
     define('BASE','/my-site/');
 
---
+++ app/site/controller/HomeController.php
@@ -0,0 +1,11 @@
+<?php
+
+    namespace app\site\controller;
+
+    class HomeController {
+
+        public function __construct() {
+
+            echo 'Teste 123';
+        }
+    }
